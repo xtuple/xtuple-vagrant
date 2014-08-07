82b45708ab06ac015be3e3a9e1d3acc77d4b3e11
jgunderson <jgunderson@erpbisolutions.com>
2014-08-07 10:00:13 -0400

    test provisioning for BI

--- bi-demo/vagrant_setup.sh
+++ bi-demo/vagrant_setup.sh
@@ -5,6 +5,10 @@ wget git.io/hikK5g -qO- | sudo bash
 n 0.8
 npm update -g npm
 
+# diagnostics
+set -x
+whoami
+
 # The FOSS xtuple-server requires an existing xtuple 
 git clone https://github.com/xtuple/xtuple.git  
 cd xtuple
@@ -25,7 +29,7 @@ cd ..
 npm install -g xtuple-server
 
 # Use the server to do an install and build xtuple
-udo xtuple-server install-dev --xt-version 4.5.1 --xt-demo --local-workspace ./xtuple --xt-adminpw admin
+sudo -u vagrant xtuple-server install-dev --xt-version 4.5.1 --xt-demo --local-workspace ./xtuple --xt-adminpw admin
 
 # One of the above sets node to 0.11.13 so back again.
 n 0.8
@@ -33,19 +37,24 @@ n 0.8
 # Install the bi-open extension. TODO: build this into the xtuple-server install as a flag
 cd xtuple
 ./scripts/build_app.js -d demo_dev -e ../xtuple-extensions/source/bi_open
+cd ..
 
-# Install BI and perform ETL
-cd ../bi-open/scripts
+# Install BI load data and start BI server
+git clone https://github.com/xtuple/bi-open.git  
+cd bi-open/scripts
+git checkout 4_6_x
 bash build_bi.sh -eblm -c ../../xtuple/node-datasource/config.js -d demo_dev -P admin
 bash start_bi.sh
+cd ../..
 
 # Start the app.
-cd ../../xtuple
+cd xtuple
 npm start > console.log &
 sleep 10
+cd ..
 
 # Run a test to make sure that BI is accessible and the ETL worked
-cd ../xtuple-extensions
+cd xtuple-extensions
 cp ../xtuple/test/lib/login_data.js test/lib/login_data.js
 npm run-script test-bi_open
 
