b220c4c3fe160443d22f661d01bae57914cf7e90
jgunderson <jgunderson@erpbisolutions.com>
2014-08-14 16:48:11 -0400

    Commercial BI demo

--- bi-demo/vagrant_provision.sh
+++ bi-demo/vagrant_provision.sh
@@ -1,51 +1,65 @@
 #!/bin/sh
-
-# Boostrap. This sets node to 0.11.13 which causes npm problems with bcrpyt and ursa.  
-wget git.io/hikK5g -qO- | sudo bash
-# So we reset to 0.8
-sudo n 0.8
-sudo npm update -g npm
-
-# diagnostics
 set -x
 
-# The FOSS xtuple-server requires an existing xtuple 
-git clone https://github.com/xtuple/xtuple.git  
-cd xtuple
-git checkout 4_6_x
-git submodule update --init --recursive --quiet
-npm install --quiet
+# Bootstrap
+wget git.io/hikK5g -qO- | sudo bash
+
+# Clone repos first.  Have trouble with git ssh authorization after xtuple-server install-dev is run (why?)
+git clone git@github.com:xtuple/xtuple-server-commercial.git
+git clone https://github.com/xtuple/xtuple.git --recursive
+git clone https://github.com/xtuple/xtuple-extensions.git
+git clone https://github.com/xtuple/bi-open.git
+git clone git@github.com:xtuple/bi.git
+git clone git@github.com:xtuple/private-extensions.git
+
+# Install xtuple-server-commercial
+cd xtuple-server-commercial
+npm install
 cd ..
 
-# And we need extensions for bi-open
-git clone https://github.com/xtuple/xtuple-extensions.git  
-cd xtuple-extensions
-git checkout 4_6_x
-git submodule update --init --recursive --quiet
-npm install --quiet
-cd ..
-
-# Install xtuple-server
-npm install -g xtuple-server
-
-# Use the server to do an install and build xtuple
-sudo xtuple-server install-dev --xt-version 4.5.1 --xt-demo --local-workspace ./xtuple --xt-adminpw admin
-
-# One of the above sets node to 0.11.13 so back again.
+# Install xtuple-extensions
+sudo chmod -R 777 /usr/local/lib
 sudo n 0.8
+cd xtuple-extensions
+git submodule update --init --recursive --quiet
+npm install --quiet
+cd ..
+  
+# Install xtuple with latest npm
+sudo npm update -g npm
+cd xtuple
+npm install --quiet
+cd ..
 
-# Install the bi-open extension. TODO: build this into the xtuple-server install as a flag
+# Use the server to do an install and build xtuple (must be in the xtuple folder?)
+cd xtuple
+sudo xtuple-server install-dev --xt-demo --xt-adminpw admin
+cd ..
+
+# Install BI and perform ETL
+sudo chmod -R 777 /usr/local/lib
+sudo n 0.8
+cd bi-open/scripts
+sudo -H bash build_bi.sh -ebm -c ../../xtuple/node-datasource/config.js -d demo_dev -P admin
+cd ../../bi/scripts
+sudo bash install.sh
+cd ../../bi-open/scripts
+sudo bash build_bi.sh -l -c ../../xtuple/node-datasource/config.js -d demo_dev -P admin
+sudo bash start_bi.sh
+cd ../..
+
+# Install bi-open.
 cd xtuple
 sudo ./scripts/build_app.js -d demo_dev -e ../xtuple-extensions/source/bi_open
 cd ..
-
-# Install BI load data and start BI server
-git clone https://github.com/xtuple/bi-open.git  
-cd bi-open/scripts
-git checkout 4_6_x
-sudo bash build_bi.sh -eblm -c ../../xtuple/node-datasource/config.js -d demo_dev -P admin
-sudo bash start_bi.sh
-cd ../..
+  
+# Install the bi commercial extension. 
+cd private-extensions
+git submodule update --init --recursive --quiet
+npm install --quiet
+cd ../xtuple
+sudo ./scripts/build_app.js -d demo_dev -e ../private-extensions/source/bi
+cd ..
 
 # Start the app.
 cd xtuple
@@ -54,11 +68,12 @@ sleep 10
 cd ..
 
 # Run a test to make sure that BI is accessible and the ETL worked
-cd xtuple-extensions
-cp ../xtuple/test/lib/login_data.js test/lib/login_data.js
-sudo npm run-script test-bi_open
+cd private-extensions
+#- cat test/lib/sample_login_data.js | sed 's#org:.*#org: \"demo_dev\",#' > test/lib/login_data.js
+cp ../xtuple/test/lib/login_data.js* test/lib/login_data.js
+sudo npm run-script test-bi
 
-# diagostics (move to after failure)
+#move to after-failure
 cat test/lib/login_data.js
 cat ../ErpBI/data-integration/properties/psg-linux/.kettle/kettle.properties
 cat ../ErpBI/biserver-ce/tomcat/logs/catalina.out
--- bi-demo/vagrant_setup.sh
+++ bi-demo/vagrant_setup.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+set -x
+
+# Copy ssh keys for private repos 
+sudo cp /home/vagrant/scripts/.ssh/* /home/vagrant/.ssh
+#sudo chown -R vagrant /home/vagrant/.ssh
+#sudo chgrp -R vagrant /home/vagrant/.ssh
+#sudo chmod -R 600 /home/vagrant/.ssh
 
 # Run provision as vagrant
 su -c "source /home/vagrant/scripts/vagrant_provision.sh" vagrant
